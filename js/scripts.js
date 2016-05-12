605fca1311a781ee97afa51ae44025cc95823390
acraig5 <acraig5@engr-131-13.eastern.ewu.edu>
2016-05-12 14:32:19 -0700

    reader Plugin added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // My Scripts
+$('article').readmore({
+    
+  speed: 1000,
+  collapsedHeight: 100,
+  blockCSS: 'display: block; width: 50%;',
+  moreLink: '<a href="#">Read This</a>',
+  lessLink: '<a href="#">Close</a>',
+    
+});
